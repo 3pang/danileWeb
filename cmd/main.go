4f3498a2483a6a9131f67bba926b3e2c3cf3409b
3pang <3032139@qq.com>
2021-03-25 00:18:15 +0800

    main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	_ "strings"
+)
 
+func login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("method:", r.Method) //获取请求方法
+	if r.Method == "GET" {
+		log.Println("GET 方法进入")
+		t, _ := template.ParseFiles("./website/helloworld.html")
+		log.Println(t.Execute(w, nil))
+	} else {
+		log.Println("POST 方法进入")
+		defer r.Body.Close()
+		con, _ := ioutil.ReadAll(r.Body)
+		fmt.Println(string(con))
+
+		var dat map[string]interface{}
+		if err := json.Unmarshal([]byte(string(con)), &dat); err == nil {
+			fmt.Println(dat)
+			fmt.Println(dat["username"])
+		} else {
+			fmt.Println(err)
+		}
+		/*
+			//请求的是登录数据，那么执行登录的逻辑判断
+			fmt.Println("username:", r.Form["username"])
+			fmt.Println("password:", r.Form["password"])*/
+	}
+}
 func main() {
-	fmt.Print("main")
+	h := http.FileServer(http.Dir("website"))
+	http.Handle("/website/", http.StripPrefix("/website/", h)) //启动静态文件服务
+	http.HandleFunc("/login", login)                           //设置访问路由
+	err := http.ListenAndServe(":9077", nil)                   //端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
