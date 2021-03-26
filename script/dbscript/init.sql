2d3edd0b45ce555199b2eb16ab5236fbfaf1085d
wangyu <3032139@qq.com>
2021-03-26 18:08:11 +0800

    mysql db

--- script/dbscript/init.sql
+++ script/dbscript/init.sql
@@ -1,13 +1,14 @@
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `age` tinyint(3) UNSIGNED NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 导出  表 mysql223.users 结构
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+                                       `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                                       `name` varchar(25) NOT NULL,
+                                       `age` tinyint(3) unsigned NOT NULL DEFAULT '0',
+                                       PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `users`
---
-
+-- 正在导出表  mysql223.users 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `name`, `age`) VALUES
 (1, '张三', 25),
 (2, '李四', 22),
